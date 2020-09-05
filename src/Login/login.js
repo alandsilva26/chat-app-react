fecd0f384610be7552176899bb00b23fd9c1ae1f
Alan Dsilva <deathbringer2604@gmail.com>
2020-09-05 17:43:41 +0530

    Add login

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,9 +1,125 @@
+import { Link } from "react-router-dom";
 import React from "react";
+import styles from "./styles";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const firebase = require("firebase");
 
 class LoginComponent extends React.Component {
-  render() {
-    return <h1>Hello from login</h1>;
+  constructor() {
+    super();
+    this.state = {
+      email: null,
+      password: null,
+      loginError: "",
+    };
   }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.main}>
+        <CssBaseline></CssBaseline>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Log In
+          </Typography>
+          <form className={classes.form} onSubmit={(e) => this.submitLogin(e)}>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="login-email-input">
+                Enter your email
+              </InputLabel>
+              <Input
+                autoComplete="email"
+                autoFocus
+                id="login-email-input"
+                onChange={(e) => this.userTyping("email", e)}
+              ></Input>
+            </FormControl>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="login-password-input">
+                Enter your password
+              </InputLabel>
+              <Input
+                id="login-password-input"
+                type="password"
+                onChange={(e) => this.userTyping("password", e)}
+              ></Input>
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Login
+            </Button>
+          </form>
+          {this.state.loginError ? (
+            <Typography
+              component="h5"
+              vairant="h6"
+              className={classes.errorText}
+            >
+              {this.state.loginError}
+            </Typography>
+          ) : null}
+          <Typography
+            component="h5"
+            variant="h6"
+            className={classes.noAccountHeader}
+          >
+            Don't Have an Account?
+          </Typography>
+          <Link className={classes.signUpLink} to="/signup">
+            Sign up
+          </Link>
+        </Paper>
+      </main>
+    );
+  }
+
+  userTyping = (type, e) => {
+    switch (type) {
+      case "email":
+        this.setState({
+          email: e.target.value,
+        });
+        break;
+      case "password":
+        this.setState({
+          password: e.target.value,
+        });
+        break;
+    }
+  };
+
+  submitLogin = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(
+        () => {
+          this.props.history.push("/dashboard");
+        },
+        (err) => {
+          console.log(err);
+          this.setState({
+            loginError: err.message,
+          });
+        }
+      );
+  };
 }
 
-export default LoginComponent;
+export default withStyles(styles)(LoginComponent);
