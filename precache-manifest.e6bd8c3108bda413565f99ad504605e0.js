b5cbc158947bd16ba93a49b4357370afb99bc468
Alan Dsilva <deathbringer2604@gmail.com>
2020-09-05 14:28:21 +0530

    Updates

---
+++ precache-manifest.e6bd8c3108bda413565f99ad504605e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93261a15ca28fa3deec111581575498",
+    "url": "/chat-app-react/index.html"
+  },
+  {
+    "revision": "2b45c5e6247ad8c2cae4",
+    "url": "/chat-app-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7bbe5cc2c9cc3055f442",
+    "url": "/chat-app-react/static/js/2.e68f6a41.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/chat-app-react/static/js/2.e68f6a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b45c5e6247ad8c2cae4",
+    "url": "/chat-app-react/static/js/main.f6c8e111.chunk.js"
+  },
+  {
+    "revision": "61eb9b8b6c9113f81abc",
+    "url": "/chat-app-react/static/js/runtime-main.2b16b2a8.js"
+  }
+]);
