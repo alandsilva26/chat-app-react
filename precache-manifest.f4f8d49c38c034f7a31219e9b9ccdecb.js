f228f5322b3da99c355320e8649b52d197542ae2
Alan Dsilva <deathbringer2604@gmail.com>
2020-09-05 15:12:35 +0530

    Updates

--- precache-manifest.f4f8d49c38c034f7a31219e9b9ccdecb.js
+++ precache-manifest.f4f8d49c38c034f7a31219e9b9ccdecb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "81b3d139daf8992b33dd8db46b5ca601",
+    "revision": "92f4e30c0f0d25f6796f46a7604c5216",
     "url": "/chat-app-react/index.html"
   },
   {
-    "revision": "3bb51233522f379c25bc",
+    "revision": "efeab00bb32e02f3ccfc",
     "url": "/chat-app-react/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "da986644c5fb0b1a429f",
-    "url": "/chat-app-react/static/js/2.579dae03.chunk.js"
+    "revision": "79c79a726ec170f29d99",
+    "url": "/chat-app-react/static/js/2.86e2d5dd.chunk.js"
   },
   {
     "revision": "5f8be6edcb631ea3c834b8459dc2c264",
-    "url": "/chat-app-react/static/js/2.579dae03.chunk.js.LICENSE.txt"
+    "url": "/chat-app-react/static/js/2.86e2d5dd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3bb51233522f379c25bc",
-    "url": "/chat-app-react/static/js/main.dd66bbfe.chunk.js"
+    "revision": "efeab00bb32e02f3ccfc",
+    "url": "/chat-app-react/static/js/main.66927523.chunk.js"
   },
   {
     "revision": "61eb9b8b6c9113f81abc",
